13560f2126697fdc92a9746150718b22a1d1a8a4
Anna Chen <106anna@gmail.com>
2019-10-25 23:38:53 -0700

    HeadStart is awesome!!!!!

--- fahr2cel.py
+++ fahr2cel.py
@@ -6,4 +6,8 @@ fahrenheit = 80
 celsius = (fahrenheit - 32.0) * 5.0 / 9.0
 print(fahrenheit, "Fahrenheit is %.2f" % celsius, "Celsius.")
 
+fahrenheit = 90
+celsius = (fahrenheit - 32.0) * 5.0 / 9.0
+print(fahrenheit, "Fahrenheit is %.2f" % celsius, "Celsius.")
+
 print("HeadStart is awesome")
